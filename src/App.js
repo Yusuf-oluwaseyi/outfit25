ac2eb9ba53e0b990807ea625ef61aad8e1a6f909
yusuf-oluwaseyi <tobacrown@gmail.com>
2024-04-11 14:26:03 +0100

    components block

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Homepage from "./Homepage.jsx";
+import Homepage from "./components/homepage/Homepage.jsx";
 
 function App() {
   return (
---
+++ src/components/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./homepage.style.scss"
+import Directory from "../directory-menu/directory-menu"
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+
+export default Homepage;
